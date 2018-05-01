d2730cf27b211f4f46b5708c9662b8aa8d99f286
desshub <desshub95@gmail.com>
2018-05-01 23:21:04 +0300

    added candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,13 +1,26 @@
 pragma solidity ^0.4.17;
 
 contract Election {
-    //Store Candidate
-    // Read candiate
+    // Model a candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
 
-    string public candidate;
+    //Store Candidate
+    uint public candidateCount;
+    // Read candidate
+    mapping(uint => Candidate) public candidates;
 
     // Constructor 
     function Election () public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string _name) private {
+        candidateCount ++;
+        candidates[candidateCount] = Candidate(candidateCount, _name, 0);
     }
 }
